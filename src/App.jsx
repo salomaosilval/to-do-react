8a05ea07eb24b0a7464d4de3527105b845075ca0
Salomão Silva <salomaosilval@hotmail.com>
2023-01-14 18:16:21 -0400

    feat: Function to delete task and icon delete

--- src/App.jsx
+++ src/App.jsx
@@ -42,11 +42,21 @@ function App() {
     setTasks(newTask);
   };
 
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+
+    setTasks(newTasks);
+  };
+
   return (
     <div>
       <div className="container">
         <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} />
+        <Tasks
+          tasks={tasks}
+          handleTaskClick={handleTaskClick}
+          handleTaskDeletion={handleTaskDeletion}
+        />
       </div>
     </div>
   );
